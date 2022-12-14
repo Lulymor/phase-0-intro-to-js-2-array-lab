6ac2c19bcbebdc73a7ccd63695190799054401f2
Luana <lugamoreira@gmail.com>
2022-12-14 18:22:23 -0500

    commit tree 03b7421caa2b0f93e4e9f246634e7062de924898

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function desctructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() { 
+    cats.shift();
+}
